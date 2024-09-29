3f09f7da505ad12a44e445084ac45280c4085311
hjdevelop <hyunjun6133@naver.com>
2024-09-30 01:01:31 +0900

    [Feat] : API 통신 인터페이스 추가

---
+++ app/src/main/java/com/sooum/android/CardApi.kt
@@ -0,0 +1,22 @@
+package com.sooum.android
+
+import com.sooum.android.enums.DistanceEnum
+import com.sooum.android.model.SortedByDistanceDataModel
+import com.sooum.android.model.SortedByLatestDataModel
+import com.sooum.android.model.SortedByPopularityDataModel
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Header
+import retrofit2.http.Query
+
+interface CardApi {
+
+    @GET("/cards/home/latest")
+    suspend fun getLatestCardList(@Header("Authorization") accessToken: String, @Query("latitude") latitude: Double, @Query("longitude") longitude: Double) : Response<SortedByLatestDataModel>
+
+    @GET("/cards/home/popular")
+    suspend fun getPopularityCardList(@Header("Authorization") accessToken : String, @Query("latitude") latitude: Double, @Query("longitude") longitude: Double) : Response<SortedByPopularityDataModel>
+
+    @GET("/cards/home/distance")
+    suspend fun getDistanceCardList(@Header("Authorization") accessToken: String, @Query("latitude") latitude: Double, @Query("longitude") longitude: Double, @Query("distanceFilter") distance : DistanceEnum)  : Response<SortedByDistanceDataModel>
+}
