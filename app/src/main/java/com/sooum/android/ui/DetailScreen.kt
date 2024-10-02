9e58af21c412676f6870e2691dcec15a3fe3db5a
leehandsub <gustjq7027@naver.com>
2024-10-02 18:09:11 +0900

    [Feat] : deatil commnet 화면 구현
    - 댓글, 좋아요 부분도 구현

--- app/src/main/java/com/sooum/android/ui/DetailScreen.kt
+++ app/src/main/java/com/sooum/android/ui/DetailScreen.kt
@@ -7,15 +7,20 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
+import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -131,10 +136,136 @@ fun DetailScreen() {
                 TagItem()
             }
         }
+        Divider(
+            color = Gray3,
+            modifier = Modifier
+                .height(0.4.dp)
+                .fillMaxWidth()
+                .padding(10.dp)
+        )
+        Row(
+            modifier = Modifier
+                .padding(end = 20.dp)
+                .align(Alignment.End),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Icon(
+                modifier = Modifier
+                    .width(24.dp)
+                    .height(24.dp),
+                painter = painterResource(R.drawable.ic_detail_heart),
+                contentDescription = "좋아요",
 
-
+                )
+            Spacer(modifier = Modifier.width(5.dp))
+            Text(
+                text = "99+",
+                fontSize = 14.sp,
+                color = Color.Black
+            )
+            Icon(
+                modifier = Modifier
+                    .padding(start = 10.dp)
+                    .width(24.dp)
+                    .height(24.dp),
+                painter = painterResource(R.drawable.ic_detail_comment),
+                contentDescription = "댓글",
+            )
+            Spacer(modifier = Modifier.width(5.dp))
+            Text(
+                text = "99+",
+                fontSize = 14.sp,
+                color = Color.Black
+            )
+        }
+        LazyRow(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 10.dp)
+        ) {
+            items(3) {
+                DeatilCommentItem()
+            }
+        }
     }
 
+
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@RequiresApi(Build.VERSION_CODES.O)
+@Composable
+fun DeatilCommentItem() {
+    Card(
+        modifier = Modifier
+            .aspectRatio(1 / 0.9f)
+            .padding( end = 10.dp, bottom = 10.dp),
+        shape = RoundedCornerShape(40.dp),
+        onClick = { }
+    ) {
+        Box(
+            modifier = Modifier
+        ) {
+            ImageLoader("https://postfiles.pstatic.net/MjAyMTAzMzFfMjgz/MDAxNjE3MTc1ODA0Mzkw.UmGHi_fsjNN1Cl0G59g0wnuU93JhTs4LZL2Z5uJVMJYg.2tT327KrHv1PRdm8wgKgHn0FaOeRGkmXzRh2wpWx_30g.JPEG.shop_marchen/white-rainforest-C04QjCIf2GQ-unsplash.jpg?type=w966")
+            Box(
+                modifier = Modifier
+                    .background(
+                        Color.Black.copy(alpha = 0.7f),
+                        shape = RoundedCornerShape(24.dp)
+                    )
+                    .align(Alignment.Center)
+                    .padding(4.dp)
+            ) {
+                Text(
+                    modifier = Modifier
+                        .align(Alignment.Center)
+                        .padding(start = 16.dp, end = 16.dp, top = 14.dp, bottom = 14.dp),
+                    text = "item.content",
+                    color = Color.White,
+                    fontSize = 16.sp,
+                    fontWeight = FontWeight.Bold,
+                    maxLines = 4,
+                    overflow = TextOverflow.Ellipsis,
+                    lineHeight = 28.8.sp,
+                )
+            }
+            Box(
+                modifier = Modifier
+                    .align(Alignment.BottomEnd)
+                    .padding(end = 26.dp, bottom = 24.dp)
+            ) {
+                Row(
+                    modifier = Modifier,
+                    horizontalArrangement = Arrangement.spacedBy(8.dp)
+                ) {
+                    InfoElement(
+                        painter = painterResource(R.drawable.ic_clock),
+                        description = "시간",
+                        count = formatTimeDifference("2024-09-27T01:41:05.112716"),
+                        isTrue = false
+                    )
+                    InfoElement(
+                        painter = painterResource(R.drawable.ic_location),
+                        description = "위치",
+                        count = formatDistanceInKm(21.899822120895017),
+                        isTrue = false
+                    )
+                    InfoElement(
+                        painter = painterResource(R.drawable.ic_heart),
+                        description = "좋아요",
+                        count = "24",
+                        isTrue = false
+                    )
+                    InfoElement(
+                        painter = painterResource(R.drawable.ic_comment),
+                        description = "댓글",
+                        count = "66",
+                        isTrue = false
+                    )
+                }
+            }
+        }
+    }
 }
 
 @Composable
