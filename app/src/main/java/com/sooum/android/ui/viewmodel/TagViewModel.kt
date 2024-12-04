64446ff04003b6ec649c09141898109522cb76e8
kznHJ <hyunjun6133@naver.com>
2024-12-04 21:48:32 +0900

    [Feat] : TagScreen ViewModel 구현

--- app/src/main/java/com/sooum/android/ui/viewmodel/TagViewModel.kt
+++ app/src/main/java/com/sooum/android/ui/viewmodel/TagViewModel.kt
@@ -1,19 +1,30 @@
 package com.sooum.android.ui.viewmodel
 
 import android.util.Log
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.sooum.android.domain.model.RecommendTagDataModel
 import com.sooum.android.domain.model.SortedByLatestDataModel
+import com.sooum.android.domain.model.TagSummaryDataModel
 import com.sooum.android.domain.usecase.tag.RecommendTagUseCase
+import com.sooum.android.domain.usecase.tag.TagSummaryUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class TagViewModel @Inject constructor(private val recommendTagUseCase: RecommendTagUseCase) : ViewModel() {
+class TagViewModel @Inject constructor(
+    private val recommendTagUseCase: RecommendTagUseCase,
+    private val tagSummaryUseCase: TagSummaryUseCase
+) : ViewModel() {
     var recommendTagList = mutableStateListOf<RecommendTagDataModel.Embedded.RecommendTag>()
+        private set
+
+    var tagSummary by mutableStateOf<TagSummaryDataModel?>(null)
         private set
 
     fun getRecommendTagList() {
@@ -22,6 +33,18 @@ class TagViewModel @Inject constructor(private val recommendTagUseCase: Recommen
                 val list = recommendTagUseCase()
                 recommendTagList.clear()
                 recommendTagList.addAll(list)
+            }
+            catch (e: Exception) {
+                Log.e("HomeViewModel", e.printStackTrace().toString())
+            }
+        }
+    }
+
+    fun getTagSummary(tagId: String) {
+        viewModelScope.launch {
+            try {
+                val summary = tagSummaryUseCase(tagId)
+                tagSummary = summary
             }
             catch (e: Exception) {
                 Log.e("HomeViewModel", e.printStackTrace().toString())
