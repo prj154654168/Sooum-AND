2ecf62401e6f24ae6eee562d30892e3565831927
kznHJ <hyunjun6133@naver.com>
2024-12-04 21:41:04 +0900

    [Feat] : 즐겨찾기 추가 / 삭제, 태그 정보 불러오기 로직 추가

--- app/src/main/java/com/sooum/android/data/repository/TagRepositoryImpl.kt
+++ app/src/main/java/com/sooum/android/data/repository/TagRepositoryImpl.kt
@@ -2,6 +2,8 @@ package com.sooum.android.data.repository
 
 import com.sooum.android.data.remote.TagAPI
 import com.sooum.android.domain.model.RecommendTagDataModel
+import com.sooum.android.domain.model.Status
+import com.sooum.android.domain.model.TagSummaryDataModel
 import com.sooum.android.domain.repository.TagRepository
 import javax.inject.Inject
 
@@ -16,5 +18,41 @@ class TagRepositoryImpl @Inject constructor(private val tagApi: TagAPI) : TagRep
         }
 
         return  recommendTagList
+    }
+
+    override suspend fun postTagFavorite(tagId: String): Status {
+        val response = tagApi.postTagFavorite(tagId)
+
+        if (response.isSuccessful) {
+            return response.body() ?: throw Exception("No body found") // 바디가 null인 경우 예외 처리
+        } else {
+            // 실패한 경우의 에러 메시지를 로그로 출력
+            val errorMessage = response.errorBody()?.string() ?: "Unknown error"
+            throw Exception("Failed to get default image: $errorMessage")
+        }
+    }
+
+    override suspend fun deleteTagFavorite(tagId: String): Status {
+        val response = tagApi.deleteTagFavorite(tagId)
+
+        if (response.isSuccessful) {
+            return response.body() ?: throw Exception("No body found") // 바디가 null인 경우 예외 처리
+        } else {
+            // 실패한 경우의 에러 메시지를 로그로 출력
+            val errorMessage = response.errorBody()?.string() ?: "Unknown error"
+            throw Exception("Failed to get default image: $errorMessage")
+        }
+    }
+
+    override suspend fun getTagSummary(tagId: String): TagSummaryDataModel {
+        val response = tagApi.getTagSummary(tagId)
+
+        if (response.isSuccessful) {
+            return response.body() ?: throw Exception("No body found") // 바디가 null인 경우 예외 처리
+        } else {
+            // 실패한 경우의 에러 메시지를 로그로 출력
+            val errorMessage = response.errorBody()?.string() ?: "Unknown error"
+            throw Exception("Failed to get default image: $errorMessage")
+        }
     }
 }
--- app/src/main/java/com/sooum/android/domain/repository/TagRepository.kt
+++ app/src/main/java/com/sooum/android/domain/repository/TagRepository.kt
@@ -1,7 +1,15 @@
 package com.sooum.android.domain.repository
 
 import com.sooum.android.domain.model.RecommendTagDataModel
+import com.sooum.android.domain.model.Status
+import com.sooum.android.domain.model.TagSummaryDataModel
 
 interface TagRepository {
     suspend fun getRecommendTag() : List<RecommendTagDataModel.Embedded.RecommendTag>
+
+    suspend fun postTagFavorite(tagId: String) : Status
+
+    suspend fun deleteTagFavorite(tagId: String) : Status
+
+    suspend fun getTagSummary(tagId: String) : TagSummaryDataModel
 }
