8455e8d4907ae0fe040ddd87ca05502533c27cb1
kznHJ <hyunjun6133@naver.com>
2024-12-04 21:40:31 +0900

    [Feat] : 즐겨찾기 추가 / 삭제, 태그 정보 불러오기 API 추가

--- app/src/main/java/com/sooum/android/data/remote/TagAPI.kt
+++ app/src/main/java/com/sooum/android/data/remote/TagAPI.kt
@@ -2,9 +2,15 @@ package com.sooum.android.data.remote
 
 import com.sooum.android.domain.model.RecommendTagDataModel
 import com.sooum.android.domain.model.RelatedTagDataModel
+import com.sooum.android.domain.model.Status
+import com.sooum.android.domain.model.TagSummaryDataModel
+import com.sooum.android.enums.ReportTypeEnum
 import retrofit2.Response
+import retrofit2.http.DELETE
 import retrofit2.http.GET
 import retrofit2.http.Header
+import retrofit2.http.POST
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface TagAPI {
@@ -17,4 +23,19 @@ interface TagAPI {
 
     @GET("/tags/recommendation")
     suspend fun getRecommendTagList() : Response<RecommendTagDataModel>
+
+    @POST("/tags/{tagId}/favorite")
+    suspend fun postTagFavorite(
+        @Path("tagId") tagId: String
+    ): Response<Status>
+
+    @DELETE("/tags/{tagId}/favorite")
+    suspend fun deleteTagFavorite(
+        @Path("tagId") tagId: String
+    ): Response<Status>
+
+    @GET("/tags/{tagId}/summary")
+    suspend fun getTagSummary(
+        @Path("tagId") tagId: String
+    ) : Response<TagSummaryDataModel>
 }
