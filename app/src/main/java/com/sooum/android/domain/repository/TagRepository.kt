007b7dc4f7d344c6b14a8a87ec7aaceccec66fd4
kznHJ <hyunjun6133@naver.com>
2024-12-07 17:10:42 +0900

    [Feat] : 즐겨찾기 태그 목록 로직 구현

--- app/src/main/java/com/sooum/android/domain/repository/TagRepository.kt
+++ app/src/main/java/com/sooum/android/domain/repository/TagRepository.kt
@@ -1,5 +1,6 @@
 package com.sooum.android.domain.repository
 
+import com.sooum.android.domain.model.FavoriteTagDataModel
 import com.sooum.android.domain.model.RecommendTagDataModel
 import com.sooum.android.domain.model.Status
 import com.sooum.android.domain.model.TagSummaryDataModel
@@ -12,4 +13,6 @@ interface TagRepository {
     suspend fun deleteTagFavorite(tagId: String) : Status
 
     suspend fun getTagSummary(tagId: String) : TagSummaryDataModel
+
+    suspend fun getFavoriteTag(last: String?) : FavoriteTagDataModel
 }
