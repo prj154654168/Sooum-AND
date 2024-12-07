312466d76798c170c008e276b2e5c45a7039ca0f
kznHJ <hyunjun6133@naver.com>
2024-12-08 04:36:44 +0900

    [Feat] : Tag Search 로직 구현

--- app/src/main/java/com/sooum/android/data/repository/TagRepositoryImpl.kt
+++ app/src/main/java/com/sooum/android/data/repository/TagRepositoryImpl.kt
@@ -4,6 +4,7 @@ import com.sooum.android.data.remote.CardApi
 import com.sooum.android.data.remote.TagAPI
 import com.sooum.android.domain.model.FavoriteTagDataModel
 import com.sooum.android.domain.model.RecommendTagDataModel
+import com.sooum.android.domain.model.SearchTagDataModel
 import com.sooum.android.domain.model.Status
 import com.sooum.android.domain.model.TagFeedDataModel
 import com.sooum.android.domain.model.TagSummaryDataModel
@@ -83,6 +84,18 @@ class TagRepositoryImpl @Inject constructor(private val tagApi: TagAPI, private 
         laskPk: Long?
     ): TagFeedDataModel {
         val response = cardApi.getTagFeed(tagId, latitude, longitude, laskPk)
+
+        if (response.isSuccessful) {
+            return response.body() ?: throw Exception("No body found") // 바디가 null인 경우 예외 처리
+        } else {
+            // 실패한 경우의 에러 메시지를 로그로 출력
+            val errorMessage = response.errorBody()?.string() ?: "Unknown error"
+            throw Exception("Failed to get default image: $errorMessage")
+        }
+    }
+
+    override suspend fun getSearchTag(keyword: String): SearchTagDataModel {
+        val response = tagApi.getSearchTag(keyword)
 
         if (response.isSuccessful) {
             return response.body() ?: throw Exception("No body found") // 바디가 null인 경우 예외 처리
--- app/src/main/java/com/sooum/android/domain/repository/TagRepository.kt
+++ app/src/main/java/com/sooum/android/domain/repository/TagRepository.kt
@@ -2,6 +2,7 @@ package com.sooum.android.domain.repository
 
 import com.sooum.android.domain.model.FavoriteTagDataModel
 import com.sooum.android.domain.model.RecommendTagDataModel
+import com.sooum.android.domain.model.SearchTagDataModel
 import com.sooum.android.domain.model.Status
 import com.sooum.android.domain.model.TagFeedDataModel
 import com.sooum.android.domain.model.TagSummaryDataModel
@@ -18,4 +19,6 @@ interface TagRepository {
     suspend fun getFavoriteTag(last: String?) : FavoriteTagDataModel
 
     suspend fun getTagFeedList(tagId: String, latitude: Double?, longitude: Double?, laskPk: Long?): TagFeedDataModel
+
+    suspend fun getSearchTag(keyword: String): SearchTagDataModel
 }
