a67f508b1de5b56f9cab53800a2b3aff45323071
kznHJ <hyunjun6133@naver.com>
2025-01-04 20:02:45 +0900

    [Feat] : notification 종류 별로 list 받는 로직 추가

--- app/src/main/java/com/sooum/android/data/repository/NotificationRepositoryImpl.kt
+++ app/src/main/java/com/sooum/android/data/repository/NotificationRepositoryImpl.kt
@@ -1,7 +1,17 @@
 package com.sooum.android.data.repository
 
+import android.util.Log
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.PagingData
+import com.sooum.android.data.paging.NotificationPagingSource
 import com.sooum.android.data.remote.NotificationApi
+import com.sooum.android.domain.model.NotificationDataModel
+import com.sooum.android.domain.model.NotificationResponse
 import com.sooum.android.domain.repository.NotificationRepository
+import com.sooum.android.enums.NotificationEnum
+import com.sooum.android.enums.NotificationTypeEnum
+import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 class NotificationRepositoryImpl @Inject constructor(private val notificationApi: NotificationApi) : NotificationRepository {
@@ -34,6 +44,78 @@ class NotificationRepositoryImpl @Inject constructor(private val notificationApi
 
         if (response.isSuccessful) {
             return response.body()?.unreadCnt ?: 0
+        } else {
+            // 실패한 경우의 에러 메시지를 로그로 출력
+            val errorMessage = response.errorBody()?.string() ?: "Unknown error"
+            throw Exception("Failed to get default image: $errorMessage")
+        }
+    }
+
+    override fun getAllUnreadNotification(): Flow<PagingData<NotificationDataModel>> {
+        return Pager(
+            config = PagingConfig(
+                pageSize = 50,
+                enablePlaceholders = false
+            ),
+            pagingSourceFactory = { NotificationPagingSource(notificationApi, NotificationEnum.UNREADALL) }
+        ).flow
+    }
+
+    override fun getCardUnreadNotification(): Flow<PagingData<NotificationDataModel>> {
+        return Pager(
+            config = PagingConfig(
+                pageSize = 50,
+                enablePlaceholders = false
+            ),
+            pagingSourceFactory = { NotificationPagingSource(notificationApi, NotificationEnum.UNREADCARD) }
+        ).flow
+    }
+
+    override fun getLikeUnreadNotification(): Flow<PagingData<NotificationDataModel>> {
+        Log.d("123", "실행됨")
+        return Pager(
+            config = PagingConfig(
+                pageSize = 50,
+                enablePlaceholders = false
+            ),
+            pagingSourceFactory = { NotificationPagingSource(notificationApi, NotificationEnum.UNREADLIKE) }
+        ).flow
+    }
+
+    override fun getAllReadNotification(): Flow<PagingData<NotificationDataModel>> {
+        return Pager(
+            config = PagingConfig(
+                pageSize = 50,
+                enablePlaceholders = false
+            ),
+            pagingSourceFactory = { NotificationPagingSource(notificationApi, NotificationEnum.READALL) }
+        ).flow
+    }
+
+    override fun getCardReadNotification(): Flow<PagingData<NotificationDataModel>> {
+        return Pager(
+            config = PagingConfig(
+                pageSize = 50,
+                enablePlaceholders = false
+            ),
+            pagingSourceFactory = { NotificationPagingSource(notificationApi, NotificationEnum.READCARD) }
+        ).flow
+    }
+
+    override fun getLikeReadNotification(): Flow<PagingData<NotificationDataModel>> {
+        return Pager(
+            config = PagingConfig(
+                pageSize = 50,
+                enablePlaceholders = false
+            ),
+            pagingSourceFactory = { NotificationPagingSource(notificationApi, NotificationEnum.READLIKE) }
+        ).flow
+    }
+
+    override suspend fun patchNotificationRead(notificationId: Long): Int {
+        val response = notificationApi.patchNotificationRead(notificationId)
+        if (response.isSuccessful) {
+            return response.code()
         } else {
             // 실패한 경우의 에러 메시지를 로그로 출력
             val errorMessage = response.errorBody()?.string() ?: "Unknown error"
--- app/src/main/java/com/sooum/android/domain/repository/NotificationRepository.kt
+++ app/src/main/java/com/sooum/android/domain/repository/NotificationRepository.kt
@@ -1,9 +1,28 @@
 package com.sooum.android.domain.repository
 
+import androidx.paging.PagingData
+import com.sooum.android.domain.model.NotificationDataModel
+import com.sooum.android.domain.model.NotificationResponse
+import kotlinx.coroutines.flow.Flow
+
 interface NotificationRepository {
     suspend fun getAllUnreadCount() : Int
 
     suspend fun getCardUnreadCount() : Int
 
     suspend fun getLikeUnreadCount() : Int
+
+    fun getAllUnreadNotification(): Flow<PagingData<NotificationDataModel>>
+
+    fun getCardUnreadNotification(): Flow<PagingData<NotificationDataModel>>
+
+    fun getLikeUnreadNotification(): Flow<PagingData<NotificationDataModel>>
+
+    fun getAllReadNotification(): Flow<PagingData<NotificationDataModel>>
+
+    fun getCardReadNotification(): Flow<PagingData<NotificationDataModel>>
+
+    fun getLikeReadNotification(): Flow<PagingData<NotificationDataModel>>
+
+    suspend fun patchNotificationRead(notificationId: Long): Int
 }
