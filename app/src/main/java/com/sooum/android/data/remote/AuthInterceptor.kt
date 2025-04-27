dd55280d9235fb5bc165a78c9f153e8d5a88544f
youngseoc <cys020628@naver.com>
2025-04-27 13:10:48 +0900

    fix: interceptor 간단하게 로직 변경

--- app/src/main/java/com/sooum/android/data/remote/AuthInterceptor.kt
+++ app/src/main/java/com/sooum/android/data/remote/AuthInterceptor.kt
@@ -1,40 +1,20 @@
 package com.sooum.android.data.remote
 
-import android.util.Log
-import com.sooum.android.Constants
+
 import com.sooum.android.SooumApplication
-import okhttp3.*
-import org.json.JSONObject
-import java.util.concurrent.TimeUnit
+import okhttp3.Interceptor
+import okhttp3.Response
 
 class AuthInterceptor : Interceptor {
-
-    private val excludedPaths = listOf(
-        "/users",
-        "/profiles"
-    )
 
     override fun intercept(chain: Interceptor.Chain): Response {
         val accessToken = SooumApplication().getVariable("accessToken")
         val request = chain.request()
-        val path = request.url.encodedPath
 
-        val shouldExclude = excludedPaths.any { path.startsWith(it) }
+        val newRequest = request.newBuilder()
+            .addHeader("Authorization", "Bearer $accessToken")
+            .build()
 
-        return if (shouldExclude) {
-            chain.proceed(request)
-        } else {
-            val newRequest = request.newBuilder()
-                .addHeader("Authorization", "Bearer $accessToken")
-                .build()
-            chain.proceed(newRequest)
-        }
-
-//        val newRequest = chain.request().newBuilder().apply {
-//            if (accessToken.isNotEmpty()) {
-//                addHeader("Authorization", "Bearer $accessToken")
-//            }
-//        }.build()
-//        return chain.proceed(newRequest)
+        return chain.proceed(newRequest)
     }
 }
