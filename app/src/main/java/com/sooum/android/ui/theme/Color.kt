1d0587edafd1859f71828ea4c939b0a1b249551d
kznHJ <hyunjun6133@naver.com>
2025-07-13 17:49:09 +0900

    [Design] : Color Component 등록

--- app/src/main/java/com/sooum/android/ui/theme/Color.kt
+++ app/src/main/java/com/sooum/android/ui/theme/Color.kt
@@ -12,16 +12,32 @@ val Pink40 = Color(0xFF7D5260)
 
 val White100 = Color(0xFFFFFFFF)
 val White70 = Color(0xB3FFFFFF)
-val GrayWhite = Color(0xFFFEFEFE)
+
 
 val Gray1 = Color(0xFF7D7D7D)
 val Gray3 = Color(0xFFC7C7C7)
 val Gray4 = Color(0xFFDEDEDE)
-val Gray50 = Color(0xFFF9F9F9)
+
 val Gray5 = Color(0xFF8D8D8D)
-val Gray300 = Color(0xFFC7C7C7)
-val Gray100 = Color(0xFFEEEEEE)
-val Gray500 = Color(0xFF8D8D8D)
 val GrayRadio = Color(0xFFC6C6C6)
 
 val Primary = Color(0xFF0BC7F2)
+
+//색상은 이 아래에 있는 것만 사용하기
+val GrayWhite = Color(0xFFFEFEFE)
+val Gray50 = Color(0xFFF9F9F9)
+val Gray100 = Color(0xFFEEEEEE)
+val Gray200 = Color(0xFFE5E5E5)
+val Gray300 = Color(0xFFC7C7C7)
+val Gray400 = Color(0xFFB5B5B5)
+val Gray500 = Color(0xFF8D8D8D)
+val Gray600 = Color(0xFF6D6D6D)
+val Gray700 = Color(0xFF545454)
+val Gray800 = Color(0xFF3A3A3A)
+val GrayBlack = Color(0xFF222222)
+
+val Primary100 = Color(0xFFEFFAFF)
+val Primary200 = Color(0xFFAFE5FD)
+val Primary300 = Color(0xFF0BC7F2)
+
+val SecondaryRed = Color(0xFFFF2424)
