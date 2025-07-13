0076f61dfe939ac22752961ce0e17824c5367800
kznHJ <hyunjun6133@naver.com>
2025-07-13 17:48:53 +0900

    [Design] : Font Component 등록

--- app/src/main/java/com/sooum/android/ui/theme/Type.kt
+++ app/src/main/java/com/sooum/android/ui/theme/Type.kt
@@ -1,34 +1,128 @@
 package com.sooum.android.ui.theme
 
-import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.em
 import androidx.compose.ui.unit.sp
+import com.sooum.android.R
 
-// Set of Material typography styles to start with
-val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+object AppTextStyles {
+    val head1Bold = TextStyle(
+        fontFamily = Pretendard,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 22.sp,
+        lineHeight = 28.sp,
+        letterSpacing = (-0.003).em,
+        color = GrayBlack
+    )
+    val head1Regular = TextStyle(
+        fontFamily = Pretendard,
+        fontWeight = FontWeight.Medium,
+        fontSize = 24.sp,
+        lineHeight = 28.sp,
+        letterSpacing = (-0.003).em,
+        color = GrayBlack
+    )
+    val head2Bold = TextStyle(
+        fontFamily = Pretendard,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 18.sp,
+        lineHeight = 24.48.sp,
+        letterSpacing = (-0.003).em,
+        color = GrayBlack
+    )
+    val head2Regular = TextStyle(
+        fontFamily = Pretendard,
+        fontWeight = FontWeight.Normal,
+        fontSize = 18.sp,
+        lineHeight = 24.48.sp,
+        letterSpacing = (-0.003).em,
+        color = GrayBlack
+    )
+    val body1Bold = TextStyle(
+        fontFamily = Pretendard,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 16.sp,
+        lineHeight = 24.sp,
+        letterSpacing = (-0.004).em,
+        color = GrayBlack
+    )
+    val body1Regular = TextStyle(
+        fontFamily = Pretendard,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
+        letterSpacing = (-0.004).em,
+        color = GrayBlack
     )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
-    ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
+    val body2Bold = TextStyle(
+        fontFamily = Pretendard,
         fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
+        fontSize = 14.sp,
+        lineHeight = 19.6.sp,
+        letterSpacing = (-0.004).em,
+        color = GrayBlack
     )
-    */
+    val body2Regular = TextStyle(
+        fontFamily = Pretendard,
+        fontWeight = FontWeight.Normal,
+        fontSize = 14.sp,
+        lineHeight = 19.6.sp,
+        letterSpacing = (-0.004).em,
+        color = GrayBlack
+    )
+    val body3Bold = TextStyle(
+        fontFamily = Pretendard,
+        fontWeight = FontWeight.Medium,
+        fontSize = 12.sp,
+        lineHeight = 16.8.sp,
+        letterSpacing = (-0.004).em,
+        color = GrayBlack
+    )
+    val body3Regular = TextStyle(
+        fontFamily = Pretendard,
+        fontWeight = FontWeight.Normal,
+        fontSize = 12.sp,
+        lineHeight = 16.8.sp,
+        letterSpacing = (-0.004).em,
+        color = GrayBlack
+    )
+    val caption = TextStyle(
+        fontFamily = Pretendard,
+        fontWeight = FontWeight.Medium,
+        fontSize = 10.sp,
+        lineHeight = 14.sp,
+        letterSpacing = (-0.004).em,
+        color = GrayBlack
+    )
+    val body1BoldHandWriting = TextStyle(
+        fontFamily = HandWriting,
+        fontWeight = FontWeight.Bold,
+        fontSize = 18.sp,
+        lineHeight = 25.2.sp,
+        letterSpacing = (-0.004).em,
+        color = GrayBlack
+    )
+    val body1RegularHandWriting = TextStyle(
+        fontFamily = HandWriting,
+        fontWeight = FontWeight.Light,
+        fontSize = 18.sp,
+        lineHeight = 25.2.sp,
+        letterSpacing = (-0.004).em,
+        color = GrayBlack
+    )
+}
+
+
+val Pretendard = FontFamily(
+    Font(R.font.pretendard_semi_bold, FontWeight.SemiBold),
+    Font(R.font.pretendard_medium, FontWeight.Medium),
+    Font(R.font.pretendard_regular, FontWeight.Normal),
+)
+
+val HandWriting = FontFamily(
+    Font(R.font.hand_write_bold, FontWeight.Bold),
+    Font(R.font.hand_write_light, FontWeight.Light),
 )
